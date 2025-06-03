0556deca045516f3e0654afceb3acfcd14a3ea03
Shawn Witemyre <switemyre@comcast.net>
2025-06-03 19:54:21 -0400

    Added logo and name to the Navbar component Styled the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,20 @@
 export default function Navbar() {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <header>
+      <nav className="navbar">
+        <ul className="navbar__list">
+          <li>
+            <div className="navbar__brand">
+              <img
+                src="public/images/react-logo.png"
+                alt="React logo"
+                className="navbar__logo"
+              />
+              <span className="navbar__name">ReactFacts</span>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
