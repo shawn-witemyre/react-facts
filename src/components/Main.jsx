499150637bef6eb03a56ff484f3599fbbc68ef62
Shawn Witemyre <switemyre@comcast.net>
2025-06-04 15:56:14 -0400

    Built out and styled main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1>Main goes here</h1>;
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was original created by Jordan Walke</li>
+        <li>Has well over 100k on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterpise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
